2cf443507782d6761af34e4d7fa1d9b7b19dc4ed
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-10-13 16:05:45 +0100

    added user roles auth

---
+++ users.js
@@ -0,0 +1,14 @@
+// [
+    //     {
+    //         "username":"Kiki1",
+    //         "password":"$2b$10$B2kyQm.lYHLn66MyYPv6vuQxhps4.Z5E8xUgO081WX5dL/dkFD3z6",
+    //         "refreshToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Iktpa2kxIiwiaWF0IjoxNjY1NTAxMzk4LCJleHAiOjE2NjU1ODc3OTh9.lzuYNi3BwXA3cvYxDBKOZQk92qH_Y7oddglcXNzoNLI"
+    //     },
+    //     {
+    //         "username":"Kiki2",
+    //         "password":"$2b$10$zkdnFx5GOuSXBxYV5F3xy.Q.KrKOODtozeQ9.iSF.JsINpDYgEusq",
+    //         "refreshToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Iktpa2kyIiwiaWF0IjoxNjY1NTAxNzQyLCJleHAiOjE2NjU1ODgxNDJ9.01gFHluiwrOYNCoq4jkOXdcCVZCXiSxcoLJUXHJsFnQ"
+    //     }
+    // ]
+
+    
