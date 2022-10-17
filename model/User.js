c2df09fd737787ac4ea09ce8bd1e000b2c10dfcc
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-10-17 14:31:47 +0100

    updated controllers with schema for mongodb

--- model/User.js
+++ model/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
     username: {
         type: String,
-        requuired: true
+        required: true
     },
     roles: {
         User: {
