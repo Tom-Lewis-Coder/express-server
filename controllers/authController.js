ea04c3f6d1299fb54a6816eba23548d20ee35dec
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-10-24 20:28:08 +0100

    updated allowed origins and authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ const handleLogin = async (req, res) => {
     // evaluate password 
     const match = await bcrypt.compare(pwd, foundUser.password)
     if (match) {
-        const roles = Object.values(foundUser.roles)
+        const roles = Object.values(foundUser.roles).filter(Boolean)
         // create JWTs
         const accessToken = jwt.sign(
             { 
@@ -33,7 +33,7 @@ const handleLogin = async (req, res) => {
         console.log(result)
 
         res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }) //secure: true,
-        res.json({ accessToken })
+        res.json({ roles, accessToken })
     } else {
         res.sendStatus(401)
     }
